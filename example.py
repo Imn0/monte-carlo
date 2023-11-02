72d996d6e36fd4440898cce788e15e5daadba07f
Krzysztof <Krzysztof.jan.andrzejewski@gmail.com>
2023-11-02 21:57:04 +0100

    added lables

--- example.py
+++ example.py
@@ -4,21 +4,15 @@ import numpy as np
 
 
 
-
-MonteCarlo(0.0, 8, np.cbrt, true_value=12, function_name="cube_root", threads=4, n_start=100000, n_end=100000)
-
-# MonteCarlo(0.0, math.pi, math.sin, true_value=2, function_name="sin", threads=4)
-
-
-
-def fun(x):
-    return 4*x*((1-x)**3)
-                # lambdas dont work
-# MonteCarlo(0.0, 1, fun, true_value=0.2, function_name="cubic_blend", threads=4)
-
-def pi_eval_fun(fun, point:(np.single, np.single)) -> int:
+def pi_eval_fun(fun, point:(np.double, np.double)) -> int:
     if math.dist((0,0),point) <= 1.0:
         return 1
     return 0
 
-# MonteCarlo(-1.0, 1.0, None, true_value=math.pi, function_name="pi", threads=4, min_val=-1.0, max_val=1.0, eval_fun=pi_eval_fun)
+def fun(x):
+    return 4*x*((1-x)**3)
+
+MonteCarlo(0.0, 8, np.cbrt, true_value=12, function_name="cube_root", threads=16, n_start=100000000, n_end=100000000)
+# MonteCarlo(0.0, math.pi, math.sin, true_value=2, function_name="sin", threads=8)
+# MonteCarlo(0.0, 1, fun, true_value=0.2, function_name="cubic_blend", threads=8)
+# MonteCarlo(-1.0, 1.0, None, true_value=math.pi, function_name="pi", threads=8, min_val=-1.0, max_val=1.0, eval_fun=pi_eval_fun)
