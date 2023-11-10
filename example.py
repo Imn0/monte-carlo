a6c76dc191f566d31979be3d084c6c61eb02b929
Krzysztof <Krzysztof.jan.andrzejewski@gmail.com>
2023-11-10 16:04:00 +0100

    changed grapher

--- example.py
+++ example.py
@@ -15,7 +15,7 @@ def fun(x):
 def cbrt(x):
     return x*(1./3.)
 
-MonteCarlo(0.0, 8, cbrt, true_value=12, function_name="cube_root", threads=4)
+MonteCarlo(0.0, 8, cbrt, function_name="cube_root", threads=4)
 MonteCarlo(0.0, math.pi, math.sin, true_value=2, function_name="sin")
 MonteCarlo(0.0, 1, fun, true_value=0.2, function_name="cubic_blend", threads=4)
 MonteCarlo(-1.0, 1.0, None, true_value=math.pi, function_name="pi", threads=4, min_val=-1.0, max_val=1.0, eval_fun=pi_eval_fun)
--- montecarlo.py
+++ montecarlo.py
@@ -8,7 +8,7 @@ def get_long_name(name, n_start, n_end, n_step, sets):
     return name + "_s" + str(n_start)+"_e"+str(n_end)+"_step"+str(n_step)+"_k"+str(sets)
     
 class MonteCarlo:
-    def __init__(self, start: np.single, stop: np.single, fun_to_integrate, true_value, 
+    def __init__(self, start: np.single, stop: np.single, fun_to_integrate, true_value = None, 
                 n_start=50, n_end=5000, n_step=50, sets=50, function_name="function", 
                 threads=1, 
                 min_val=None, max_val=None, eval_fun=None,
@@ -23,16 +23,18 @@ class MonteCarlo:
         carlo = Carlo(start, stop, fun_to_integrate,
                          n_start, n_end, n_step, sets, threads, min_val, max_val, eval_fun, eps)
         
+        print("calculating")
+        all_results, avg_results = carlo()
+        avg_all = np.mean(all_results)
         print("creating grapher")
+        if true_value == None:
+            true_value = avg_all
+
         monte = Monte(true_value, n_start, n_end, n_step, sets, function_name, long_name, save_graph, show_graph)
         
-        print("calculating")
-        monte(carlo())
+        monte(all_results, avg_results)
 
         print("saving results")
-        all_results = carlo.all_results
-        avg_results = carlo.avg_results
-        avg_all = np.mean(all_results)
         all_results.tofile(f'./results/{function_name}/all_{long_name}.csv', sep = ';')
         avg_results.tofile(f'./results/{function_name}/avg_{long_name}.csv', sep = ';')
         avg_all.tofile(f'./results/{function_name}/avg_all_{long_name}.csv', sep = ';')
